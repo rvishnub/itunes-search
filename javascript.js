10141a1cd083164ddc3d0dde4ccbaca3a96dbd06
rvishnub <rvishnub@yahoo.com>
2016-09-30 12:27:08 -0500

    youtube search works, trying to embed video

--- javascript.js
+++ javascript.js
@@ -4,6 +4,28 @@ $(document).ready(function(){
     });
 });
 
+function performSearch()
+{
+	var terms = jQuery('#term').val();
+	if (terms==""){
+		alert("Please enter a valid search item.");
+	};
+	$.ajax({
+	"url": "https://itunes.apple.com/search?", 
+	"type": "GET",
+	"dataType": "JSONP",
+	"data": {"term": terms},
+	"success": function(data) {
+		$('#pagination-container').pagination({
+		dataSource: data.results,
+		callback: function(data, pagination) {
+        var html = displayTable(data);
+        $('#data-container').html(html);
+    }
+})},})
+}
+/*lines 19-23 from pagination plugin from paginationjs.com, MIT License (MIT) Copyright (c) 2014-2048*/
+
 /* to see list, not table */
 function displayTracks(data)
 {
@@ -22,39 +44,9 @@ function displayTracks(data)
 	document.getElementById("songlist").innerHTML = songList;
 }
 
-function performSearch()
-{
-	var terms = jQuery('#term').val();
-	if (terms==""){
-		alert("Please enter a valid search item.");
-	};
-	$.ajax({
-	"url": "https://itunes.apple.com/search?", 
-	"type": "GET",
-	"dataType": "JSONP",
-	"data": {"term": terms},
-	"success": function(data) {
-		console.log(data);
-		$('#pagination-container').pagination({
-		dataSource: data.results,
-		callback: function(data, pagination) {
-        var html = displayTable(data);
-        $('#data-container').html(html);
-    }
-})},})
-}
-/*lines 36-40 from pagination plugin from http://paginationjs.com/*/
 
-function performNapsterSearch()
-{
-	var terms = jQuery('#term').val();
-	$.ajax({
-	"url": "https://api.napster.com/v2.0/search?", 
-	"type": "GET",
-	"dataType": "JSONP",
-	"data": {"q": terms},
-	"success": function(data) {displayTable(data.results)},})
-}
+
+
 
 function reloadPage() 
 {
@@ -64,7 +56,8 @@ function reloadPage()
 function displayTable(data) 
 {
     var divText = "";
-	$.each(data, function(index, item){
+	$.each(data, function(index, item)
+	{
 		if (item.trackPrice <0)
 		{
 			item.trackPrice = "N/A";
@@ -76,16 +69,6 @@ function displayTable(data)
     divText += "</table>";
     return divText;
 }
-
-/* function displayAlbumTable(data) 
-{
-	$.each(data, function(index, item){
-		divAlbum += "<table border=1><tr><th>" + "<a href = '"+item.collectionViewUrl+"'><img src='"+item.artworkUrl100+"'></a>" +"</th><th>"+item.collectionName++"</th><th style='text-align:center'>" + item.collectionPrice+ "</th></tr>";
-		});
-    divAlbum += "</table>";
-    $('#data-container2').html(divAlbum);
-    }
-})} */
 
 
 
--- javascript_video.js
+++ javascript_video.js
@@ -6,62 +6,56 @@ $(document).ready(function(){
 
 function performVideoSearch()
 {
-	var terms = jQuery('#term').val();
-	if (terms==""){
+	var q = jQuery('#term').val();
+	if (q=="")
+	{
 		alert("Please enter a valid search item.");
 	};
-	$.ajax({
-	"url": "https://www.googleapis.com/youtube/v3/search?key=AIzaSyA5_5cy7lwaLAt9wwiWZAYdVE1bJizhDPE&part=snippet", 
-	"type": "GET",
-	"dataType": "JSONP",
-	"data": {"term": terms},
-	"success": function(data) {
-		console.log(data);
-		$('#pagination-container').pagination({
-		dataSource: data.results,
-		callback: function(data, pagination) {
-        var html = displayTable(data);
-        $('#data-container').html(html);
-    }
-})},})
+	$.ajax(
+	{
+		"url": "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=AIzaSyDn2MOz8IoFgXpoA2APTLePIIO3MOf70y0", 
+		"type": "GET",
+		"dataType": "JSONP",
+		"data": {"q": q},
+		
+		"success": function(data) 
+		{
+			$('#pagination-container').pagination({
+			dataSource: data.items,
+			callback: function(data, pagination) {
+			var tableText = displayTable(data);
+			$('#data-container').html(tableText);
+			}
+		})
+	},
+})
+	
+/*lines 23-27 and corresponding code in html are from from pagination plugin from paginationjs.com, MIT License (MIT) Copyright (c) 2014-2048*/
+	
+		
+function displayTable(response)
+	{
+	var tableText="";
+	for (var vidFieldNum in response)
+	{
+		var item = response[vidFieldNum];
+		if (item.id.videoId == null)
+		{
+			item.id.videoId = "N/A";
+		}
+	};
+	for(var vidFieldNum in response) 
+	{
+		var item = response[vidFieldNum];
+		tableText += "<table border=1><tr><th>" + item.snippet.title + "</th><th style='text-align:center'>"+ item.id.videoId+"</th><th style='text-align:center'><a href = 'https://www.youtube.com/watch?v="+item.id.videoId+"'  target='_blank' >Go to video</a>"+"</th></tr>";
+		
+	}
+	return tableText;
+	}
 }
-/*lines 36-40 from pagination plugin from http://paginationjs.com/*/
 
-function performNapsterSearch()
-{
-	var terms = jQuery('#term').val();
-	$.ajax({
-	"url": "https://api.napster.com/v2.0/search?", 
-	"type": "GET",
-	"dataType": "JSONP",
-	"data": {"q": terms},
-	"success": function(data) {displayTable(data.results)},})
-}
 
 function reloadPage() 
 {
     location.reload();
 }
-
-function displayTable(data) 
-{
-    var divText = "";
-	$.each(data, function(index, item){
-		if (item.trackPrice <0)
-		{
-			item.trackPrice = "N/A";
-		}
-	});
-    $.each(data, function(index, item){
-        divText += "<table border=1><tr><th>" + "<img src='"+item.artworkUrl60+"'>" +"</th><th>" + item.artistName + "</th><th>"+ item.collectionName+"</th><th>"+ "<a href = '"+item.previewUrl+"' target='_blank'>"+item.trackName+"</a>"+"</th><th style='text-align:center'>" + item.trackPrice+ "</th></tr>";
-    });
-    divText += "</table>";
-    return divText;
-}
-
-
-
-
-
-
-
